6f7d976b735327025b26eddf0fabd51a03bc33a2
cipher0x9 <cipher0x9@gmail.com>
2025-12-08 21:22:36 -0500

    Create wagmi config file with Base and mainnet support

---
+++ lib/wagmi.ts
@@ -0,0 +1,10 @@
+import { http, createConfig } from 'wagmi';
+import { base, mainnet } from 'wagmi/chains';
+
+export const wagmiConfig = createConfig({
+  chains: [mainnet, base],
+  transports: {
+    [mainnet.id]: http(),
+    [base.id]: http(),
+  },
+});
